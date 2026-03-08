2944b2f1f40e5253799352fbee76a33d5e90fdbd
huytn593 <huytn593@gmail.com>
2026-03-08 21:08:10 +0700

    Delete utils directory

--- utils/IdHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Tim ID lon nhat trong mang du lieu
- */
-function findMaxId(arr) {
-    const allIds = arr.map(item => item.id);
-    return Math.max(...allIds);
-}
-
-module.exports = { findMaxId };
